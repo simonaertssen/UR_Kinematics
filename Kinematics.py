152a3cdff0e7583ed36b7a2e97780a047c1f7934
Simon Aertssen <simonaertssen@gmail.com>
2020-09-21 09:42:15 +0200

    tmp svae

--- Kinematics.py
+++ Kinematics.py
@@ -2,7 +2,7 @@ import numpy as np
 import time
 
 
-def T(d, theta, r, alpha):
+def T(theta, d, r, alpha):
     cos_t = np.cos(theta)
     sin_t = np.sin(theta)
     cos_a = np.cos(alpha)
@@ -16,33 +16,55 @@ def T(d, theta, r, alpha):
 class ForwardKinematics(object):
     # Use slots for faster access:
     # https://stackoverflow.com/questions/472000/usage-of-slots
-    
+    __slots__ = ('shoulder', 'elbow', 'elbow2', 'wrist1', 'wrist2', 'wrist3', 'tool')
+
     def __init__(self):
         super(ForwardKinematics).__init__()
-        self.shoulder = T(0.089159, 0, 0.134, np.pi / 2)
-        self.elbow    = T(0, -np.pi / 2, -0.425, 0)
-        self.elbow2   = T(0.119, 0, 0, 0)
-        Shoulder[0, 3], Shoulder[1, 3] = -Shoulder[1, 3], Shoulder[0, 3]
+        self.shoulder = np.zeros((4, 4))
+        self.elbow    = np.zeros((4, 4))
+        self.elbow2   = np.zeros((4, 4))
+        self.wrist1   = np.zeros((4, 4))
+        self.wrist2   = np.zeros((4, 4))
+        self.wrist3   = np.zeros((4, 4))
+        self.tool     = np.zeros((4, 4))
+
+    def setJointAngles(self, jointangles):
+        a, b, c, d, e, f = jointangles
+        self.shoulder = T(a, 0.089159, 0.134, np.pi / 2)
+        self.elbow    = T(b, 0, -0.425, 0)
+        self.elbow2   = T(b, 0.119, 0, 0)
+        self.wrist1   = T(c, 0, -0.39225, 0)
+        self.wrist2   = T(d, 0.09475, 0, np.pi / 2)
+        self.wrist3   = T(e, 0.09475, 0, -np.pi / 2)
+        self.tool     = T(f, 0.0815, 0, 0)
+
+    def forward(self, angles):
+        self.shoulder[0, 3], self.shoulder[1, 3] = -self.shoulder[1, 3], self.shoulder[0, 3]
+        self.shoulder = self.shoulder.dot(self.elbow)
+        self.elbow = self.elbow.dot(self.elbow)
+        self.elbow2 = self.elbow2.dot(self.elbow2)
+        self.wrist1 = self.wrist1.dot(self.wrist1)
+        self.wrist2 = self.wrist2.dot(self.wrist2)
+        self.wrist3 = self.wrist2.dot(self.wrist3)
+        self.tool = self.wrist3.dot(self.tool)
 
 
         # Elbow    = T(0, theta_shoulder, 0.425, 0)
         Elbow = Shoulder @ Elbow
 
-        Elbow2 = T(0.119, 0, 0, 0)
         # Elbow2   = T(0.119, theta_elbow, 0, 0)
         Elbow2 = Elbow @ Elbow2
 
         # Wrist1   = T(0, -np.pi/2, 0.39225, 0)
-        Wrist1 = T(0, 0, -0.39225, 0)
         Wrist1 = Elbow2 @ Wrist1
 
-        Wrist2 = T(0.09475, -np.pi / 2, 0, np.pi / 2)
+
         Wrist2 = Wrist1 @ Wrist2
 
-        Wrist3 = T(0.09475, 0, 0, -np.pi / 2)
+
         Wrist3 = Wrist2 @ Wrist3
 
-        Tool = T(0.0815, 0, 0, 0)
+
         Tool = Wrist3 @ Tool
         d = [0.089159]
         r = []
