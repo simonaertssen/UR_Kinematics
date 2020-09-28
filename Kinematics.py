73a0e7161a239dddddfb54e0d6f09a20a4cdc6c8
Simon Aertssen <simonaertssen@gmail.com>
2020-09-28 14:47:17 +0200

    changed class approach to pure function approach, slower but more simple

--- Kinematics.py
+++ Kinematics.py
@@ -13,54 +13,35 @@ def T(theta, d, r, alpha):
                      [    0,           0,           0,         1]])
 
 
-class ForwardKinematics(object):
-    # Use slots for faster access:
-    # https://stackoverflow.com/questions/472000/usage-of-slots
-    __slots__ = ('base', 'shoulder', 'elbow', 'elbowend', 'wrist1', 'wrist2', 'wrist3')
+def ForwardKinematics(joint_angles):
+    a, b, c, d, e, f = joint_angles
+    # The joint parameters a, d and alpha can be found here: https://www.universal-robots.com/articles/ur-articles/parameters-for-calculations-of-kinematics-and-dynamics/
+    base     = T(theta=a, d=0.089159, r=0.134,    alpha=np.pi / 2)
+    shoulder = T(theta=b, d=0,        r=-0.425,   alpha=0)
+    elbow    = T(theta=c, d=0.119,    r=0,        alpha=0)
+    elbowend = T(theta=c, d=0,        r=-0.39225, alpha=0)
+    wrist1   = T(theta=d, d=-0.09475, r=0,        alpha=np.pi / 2)
+    wrist2   = T(theta=e, d=0.09475,  r=0,        alpha=-np.pi / 2)
+    wrist3   = T(theta=f, d=-0.0815,  r=0,        alpha=0)
 
-    def __init__(self):
-        super(ForwardKinematics).__init__()
-        self.base     = np.zeros((4, 4))
-        self.shoulder = np.zeros((4, 4))
-        self.elbow    = np.zeros((4, 4))
-        self.elbowend = np.zeros((4, 4))
-        self.wrist1   = np.zeros((4, 4))
-        self.wrist2   = np.zeros((4, 4))
-        self.wrist3   = np.zeros((4, 4))
+    base[0, 3], base[1, 3] = -base[1, 3], base[0, 3]
+    shoulder = base.dot(shoulder)
+    elbow = shoulder.dot(elbow)
+    elbowend = elbow.dot(elbowend)
+    wrist1 = elbowend.dot(wrist1)
+    wrist2 = wrist1.dot(wrist2)
+    wrist3 = wrist2.dot(wrist3)
 
-    def positions(self):
-        X = np.array([0,               0, self.base[0, 3], self.shoulder[0, 3], self.elbow[0, 3], self.elbowend[0, 3], self.wrist1[0, 3], self.wrist2[0, 3], self.wrist3[0, 3]])
-        Y = np.array([0,               0, self.base[1, 3], self.shoulder[1, 3], self.elbow[1, 3], self.elbowend[1, 3], self.wrist1[1, 3], self.wrist2[1, 3], self.wrist3[1, 3]])
-        Z = np.array([0, self.base[2, 3], self.base[2, 3], self.shoulder[2, 3], self.elbow[2, 3], self.elbowend[2, 3], self.wrist1[2, 3], self.wrist2[2, 3], self.wrist3[2, 3]])
-        return X, Y, Z
-
-    def forward(self, jointangles):
-        a, b, c, d, e, f = jointangles
-        # The joint parameters a, d and alpha can be found here: https://www.universal-robots.com/articles/ur-articles/parameters-for-calculations-of-kinematics-and-dynamics/
-        self.base     = T(theta=a, d=0.089159,  r=0.134,    alpha=np.pi / 2)
-        self.shoulder = T(theta=b, d=0,         r=-0.425,   alpha=0)
-        self.elbow    = T(theta=c, d=0.119,     r=0,        alpha=0)
-        self.elbowend = T(theta=c, d=0,         r=-0.39225, alpha=0)
-        self.wrist1   = T(theta=d, d=-0.09475,  r=0,        alpha=np.pi / 2)
-        self.wrist2   = T(theta=e, d=0.09475,   r=0,        alpha=-np.pi / 2)
-        self.wrist3   = T(theta=f, d=-0.0815,   r=0,        alpha=0)
-
-        self.base[0, 3], self.base[1, 3] = -self.base[1, 3], self.base[0, 3]
-        self.shoulder = self.base.dot(self.shoulder)
-        self.elbow    = self.shoulder.dot(self.elbow)
-        self.elbowend = self.elbow.dot(self.elbowend)
-        self.wrist1   = self.elbowend.dot(self.wrist1)
-        self.wrist2   = self.wrist1.dot(self.wrist2)
-        self.wrist3   = self.wrist2.dot(self.wrist3)
-
-        return self.positions()
+    X = np.array([0, 0, base[0, 3], shoulder[0, 3], elbow[0, 3], elbowend[0, 3], wrist1[0, 3], wrist2[0, 3], wrist3[0, 3]])
+    Y = np.array([0, 0, base[1, 3], shoulder[1, 3], elbow[1, 3], elbowend[1, 3], wrist1[1, 3], wrist2[1, 3], wrist3[1, 3]])
+    Z = np.array([0, base[2, 3], base[2, 3], shoulder[2, 3], elbow[2, 3], elbowend[2, 3], wrist1[2, 3], wrist2[2, 3], wrist3[2, 3]])
+    return X, Y, Z
 
 
 def SpeedOfCurrentKinematics():
-    fk = ForwardKinematics()
     start = time.time()
     for _ in range(100000):
-        pos = fk.forward([0, -np.pi/2, 0, -np.pi/2, 0, 0])
+        pos = ForwardKinematics([0, -np.pi/2, 0, -np.pi/2, 0, 0])
     interval = time.time() - start
     print(100000/interval, "iterations per second")
 
@@ -70,3 +51,4 @@ if __name__ == '__main__':
     # 17945 iterations per second without 'slots'
     # 17859 iterations per second with 'slots'
     # 17663 iterations per second without for loop
+    # 14195 iterations per second by using just a function
