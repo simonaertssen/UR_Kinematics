3c76add72b68b356ede65934bfade6013b8c1cda
Simon Aertssen <simonaertssen@gmail.com>
2020-10-12 10:34:01 +0200

    Used numba

--- Kinematics.py
+++ Kinematics.py
@@ -1,22 +1,30 @@
 import numpy as np
 import time
+from numba import jit
 
 
+@jit(nopython=True)
 def T(theta, d, r, alpha):
     cos_t = np.cos(theta)
     sin_t = np.sin(theta)
     cos_a = np.cos(alpha)
     sin_a = np.sin(alpha)
-    return np.array([[cos_t, -sin_t*cos_a,  sin_t*sin_a, r*cos_t],
-                     [sin_t,  cos_t*cos_a, -cos_t*sin_a, r*sin_t],
-                     [    0,        sin_a,        cos_a,       d],
-                     [    0,           0,           0,         1]])
+    result = np.zeros((4, 4), np.float64)
+    result[0, 0], result[0, 1], result[0, 2], result[0, 3] = cos_t, -sin_t*cos_a,  sin_t*sin_a, r*cos_t
+    result[1, 0], result[1, 1], result[1, 2], result[1, 3] = sin_t,  cos_t*cos_a, -cos_t*sin_a, r*sin_t
+    result[2, 1], result[2, 2], result[2, 3] = sin_a, cos_a, d
+    result[4, 4] = 1
+    return result
 
 
-def ForwardKinematics(joint_angles, tool_position):
-    a, b, c, d, e, f = joint_angles
-    x, y, z = tool_position
-    # print([np.round(angle * 180/3.14) for angle in joint_angles])
+@jit(nopython=True)
+def ForwardKinematics(joint_angles, tool_position=None):
+    a, b, c, d, e, f = np.float64(joint_angles)
+    if tool_position is not None:
+        x, y, z = tool_position
+    else:
+        x, y, z = np.nan, np.nan, np.nan
+
     # The joint parameters a, d and alpha can be found here: https://www.universal-robots.com/articles/ur-articles/parameters-for-calculations-of-kinematics-and-dynamics/
     base     = T(theta=a, d=0.089159, r=-0.134,    alpha=np.pi / 2)
     shoulder = T(theta=b, d=0,        r=-0.425,   alpha=0)
@@ -36,22 +44,6 @@ def ForwardKinematics(joint_angles, tool_position):
     X = np.array([0, 0, base[0, 3], shoulder[0, 3], elbow[0, 3], elbowend[0, 3], wrist1[0, 3], wrist2[0, 3], wrist3[0, 3], x])
     Y = np.array([0, 0, base[1, 3], shoulder[1, 3], elbow[1, 3], elbowend[1, 3], wrist1[1, 3], wrist2[1, 3], wrist3[1, 3], y])
     Z = np.array([0, base[2, 3], base[2, 3], shoulder[2, 3], elbow[2, 3], elbowend[2, 3], wrist1[2, 3], wrist2[2, 3], wrist3[2, 3], z])
-
-    # base     = T(theta=a, d=0.089159, r=0,        alpha=np.pi / 2)
-    # shoulder = T(theta=b, d=0,        r=-0.425,   alpha=0)
-    # elbow    = T(theta=c, d=0,        r=-0.39225, alpha=0)
-    # wrist1   = T(theta=d, d=0.10915,  r=0,        alpha=np.pi / 2)
-    # wrist2   = T(theta=e, d=0.09465,  r=0,        alpha=-np.pi / 2)
-    # wrist3   = T(theta=f, d=0.0823,   r=0,        alpha=0)
-    # shoulder = base.dot(shoulder)
-    # elbow = shoulder.dot(elbow)
-    # wrist1 = elbow.dot(wrist1)
-    # wrist2 = wrist1.dot(wrist2)
-    # wrist3 = wrist2.dot(wrist3)
-    # X = np.array([0, 0, base[0, 3], shoulder[0, 3], elbow[0, 3], wrist1[0, 3], wrist2[0, 3], wrist3[0, 3]])
-    # Y = np.array([0, 0, base[1, 3], shoulder[1, 3], elbow[1, 3], wrist1[1, 3], wrist2[1, 3], wrist3[1, 3]])
-    # Z = np.array([0, base[2, 3], base[2, 3], shoulder[2, 3], elbow[2, 3], wrist1[2, 3], wrist2[2, 3], wrist3[2, 3]])
-    # print(X[-1], Y[-1], Z[-1])
     return X, Y, Z
 
 
@@ -70,13 +62,17 @@ def detectCollision(positions):
         return True
     return False
 
-
+# 'f8(f8, f8, f8, f8)',
 def SpeedOfCurrentKinematics():
     start = time.time()
-    for _ in range(100000):
-        pos = ForwardKinematics([0, -np.pi/2, 0, -np.pi/2, 0, 0])
+    ForwardKinematics.inspect_types()
+    n = 100000
+    for _ in range(n):
+        toolTip = None
+        pos = ForwardKinematics((0.0, -np.pi/2, 0.0, -np.pi/2, 0.0, 0.0))
     interval = time.time() - start
-    print(100000/interval, "iterations per second")
+
+    print(n/interval, "iterations per second")
 
 
 if __name__ == '__main__':
@@ -85,3 +81,4 @@ if __name__ == '__main__':
     # 17859 iterations per second with 'slots'
     # 17663 iterations per second without for loop
     # 14195 iterations per second by using just a function
+    # 54986 iterations per second by using basic Numba, increasing for longer periods
