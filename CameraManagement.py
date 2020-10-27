883b3206ec2f1edf24cb35e0f57cfa6ce5497fa0
simonaertssen <Simonaertssen@gmail.com>
2020-10-27 09:37:11 +0100

    Added TopCam without being tested

--- CameraManagement.py
+++ CameraManagement.py
@@ -1,8 +1,8 @@
 import time
+from queue import Queue
+
 import cv2 as cv
 import numpy as np
-
-from queue import Queue
 from pypylon import pylon, genicam
 
 
@@ -176,6 +176,64 @@ class CameraArray:
         except genicam.RuntimeException as e:
             print('Runtime Exception: {}'.format(e))
             return -1
+
+
+class TopCamera(Camera):
+    def __init__(self, serial_number=None, grayscale=True):
+        super(TopCamera, self).__init__(serial_number, grayscale)
+        self.TopView = np.zeros(self.getShape(), dtype=np.uint8)
+
+    @staticmethod
+    def extractInfo(image_to_extract):
+        _, contours, hierarchy = cv.findContours((image_to_extract < 20).astype(np.uint8), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        output = list()
+        for contour in contours:
+            M = cv.moments(contour)
+            if M['m00'] == 0:
+                break
+            X = int(M['m10'] / M['m00'])
+            Y = int(M['m01'] / M['m00'])
+            rect = cv.minAreaRect(contour)
+            box = cv.boxPoints(rect)
+            box = np.int0(box)
+            # Find midpoints:
+            midX = [np.int0((box[i - 1, 0] + box[i, 0]) / 2) for i in range(4)]
+            midY = [np.int0((box[i - 1, 1] + box[i, 1]) / 2) for i in range(4)]
+            # Find longest side:
+            candidates = np.sqrt(np.array([(midX[i] - midX[j]) ** 2 + (midY[i] - midY[j]) ** 2 for i, j in zip([0, 1], [2, 3])]))
+            longest_side = candidates.argmax()
+            sign = np.sign(midX[longest_side + 2] - midX[longest_side])
+            # Find angle:
+            pts = np.array([[midX[i], midY[i]] for i in longest_side + [0, 2]])
+            angle = sign * np.arccos(np.abs(pts[1] - pts[0]).dot(np.array([1, 0])) / np.linalg.norm(pts[0] - pts[1]))
+            output.append((X, Y, angle*180/np.pi))
+        return output
+
+    def grabImage(self):
+        self.Open()
+        grabbedImage = None
+        TopView = np.zeros(self.getShape(), dtype=np.uint8)
+        if not self.camera.IsGrabbing():
+            self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly, pylon.GrabLoop_ProvidedByInstantCamera)
+        try:
+            if self.camera.WaitForFrameTriggerReady(0, pylon.TimeoutHandling_Return):
+                self.camera.ExecuteSoftwareTrigger()
+            max_tries = 5
+            for i in range(max_tries):
+                cam_num, grabbedImage = self.imageEventHandler.imageQueue.get()
+                if grabbedImage is not None:
+                    break
+                elif i == max_tries-1:
+                    raise ValueError('Too many tries on one image.')
+            np.copyto(TopView, self.formatImage(grabbedImage))
+            return TopView, self.extractInfo(TopView)
+
+        except genicam.RuntimeException as e:
+            print('Runtime Exception: {}'.format(e))
+            return None
+        except ValueError as e:
+            print('Value Exception: {}'.format(e))
+            return None
 
 
 def runSingleCamera():
