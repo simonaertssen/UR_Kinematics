2d13256594b5afc3a5560e23f85836ac0de3d14e
simonaertssen <Simonaertssen@gmail.com>
2021-01-04 17:22:30 +0100

    Adding a better stop functionality

--- Corpus.py
+++ Corpus.py
@@ -1,6 +1,6 @@
 import time
 
-from threading import Thread, Event
+from threading import Thread, Event, enumerate as list_threads
 from queue import Queue
 
 from RobotClass import Robot
@@ -13,13 +13,15 @@ class MainManager:
         self.Robot = Robot
         self.TopCamera = TopCamera
         self.DetailCamera = DetailCamera
-        self.StopEvent = Event()
 
+        self.StopImageTaskEvent = Event()
         self.ImageQueue = Queue()
-        self.ImageTaskThread = Thread(target=self.runImageTasks, args=[self.ImageQueue, self.StopEvent], daemon=True, name='Async Robot tasks')
+        self.ImageInfo = None
+        self.ImageTaskThread = Thread(target=self.runImageTasks, args=[self.ImageQueue, self.StopImageTaskEvent], daemon=True, name='Fetch image from camera')
 
+        self.StopRobotTaskEvent = Event()
         self.RobotTaskQueue = Queue()
-        self.RobotTaskThread = Thread(target=self.runRobotTasks, args=[self.RobotTaskQueue, self.Robot.StopEvent], daemon=True, name='Async Robot tasks')
+        self.RobotTaskThread = Thread(target=self.runRobotTasks, args=[self.RobotTaskQueue, self.StopRobotTaskEvent], daemon=True, name='Async Robot tasks')
 
         self.tryConnect()
         self.ImageTaskThread.start()
@@ -31,7 +33,6 @@ class MainManager:
 
         def startAsync(this, error_queue, constructor):
             try:
-                print(constructor.__name__)
                 setattr(this, constructor.__name__, constructor())
             except Exception as e:
                 # Startup of this part has failed and we need to shutdown all parts
@@ -43,21 +44,19 @@ class MainManager:
 
         # Get first error, if any, and raise it to warn the instance and the parent
         if not ReturnErrorMessageQueue.empty():
-            print('There is an error')
             self.shutdownSafely()
             raise ReturnErrorMessageQueue.get()
 
     def shutdownSafely(self):
-        if not self.StopEvent.isSet():
-            self.StopEvent.set()
+        print("Active threads: ", [t.name for t in list_threads()])
         if self.RobotTaskThread.is_alive():
-            self.Robot.StopEvent.set()
+            self.StopRobotTaskEvent.set()
             self.RobotTaskThread.join()
-            self.Robot.StopEvent.clear()
-        if self.RobotTaskThread.is_alive():
-            self.StopEvent.set()
+            self.StopRobotTaskEvent.clear()
+        if self.ImageTaskThread.is_alive():
+            self.StopImageTaskEvent.set()
             self.ImageTaskThread.join()
-            self.StopEvent.clear()
+            self.StopImageTaskEvent.clear()
 
         def shutdownAsync(part):
             if part is None:
@@ -77,18 +76,28 @@ class MainManager:
                 image, info, cam_num = self.TopCamera.grabImage()
                 if image is None:
                     continue
-                image_queue.put(image, info, cam_num)
+                while not image_queue.empty():
+                    image_queue.get()
+                self.ImageInfo = info
+                image_queue.put((image, info, cam_num))
             except Exception as e:
                 print('An exception occurred while retrieving images: {}'.format(e))
 
     @staticmethod
     def runRobotTasks(robot_task_queue, robot_stop_event):
         while not robot_stop_event.is_set():
-            task_handle = robot_task_queue.get()
+            if robot_task_queue.empty():
+                time.sleep(0.001)
+                continue
+
+            task_handle = robot_task_queue.get(block=False)
+            print(task_handle, callable(task_handle))
             try:
                 task_handle(robot_stop_event)
             except TypeError as e:
                 print('An uncallable function was encountered: {}'.format(e))
+            finally:
+                robot_task_queue.task_done()
 
     def isConnected(self):
         answer = [False]*4
@@ -114,17 +123,24 @@ class MainManager:
     def closeGripper(self):
         self.giveRobotParallelTask(self.Robot.closeGripper)
 
-    def startRobot(self, info):
+    def startRobot(self):
         def startRobotHandle(stop_event_as_argument):
-            self.Robot.pickUpObject(stop_event_as_argument, info)
+            r"""" Feed the first found object into the pickup function. """
+            self.Robot.pickUpObject(stop_event_as_argument, self.ImageInfo[0])
+            print("Picked up")
             self.Robot.goHome(stop_event_as_argument)
+            print("Went Home")
             self.Robot.dropObject(stop_event_as_argument)
             self.Robot.goHome(stop_event_as_argument)
-
         self.giveRobotParallelTask(startRobotHandle)
 
     def stopRobot(self):
-        self.giveRobotParallelTask(self.Robot.halt)
+        self.Robot.StopEvent.isSet()  # Halt the execution of the current task
+
+        def stopAndReturn(stop_event_as_argument):
+            self.Robot.halt(stop_event_as_argument)
+            self.Robot.goHome(stop_event_as_argument)
+        self.giveRobotParallelTask(stopAndReturn)
 
 
 if __name__ == '__main__':
